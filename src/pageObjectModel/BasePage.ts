6c21209733632ebbf4dd7cbb4287ba5c3a6ff5e8
speshevski <stefan.peshevski@hotmail.com>
2023-09-16 21:57:55 +0200

    add methods in the BasePage

--- src/pageObjectModel/BasePage.ts
+++ src/pageObjectModel/BasePage.ts
@@ -1,5 +1,32 @@
-class BasePage {
+import {Timeout} from "../utils/Timeout";
 
+abstract class BasePage {
+    protected async waitForElementToBeDisplayed(selector: WebdriverIO.Element): Promise<void> {
+        await selector.waitForDisplayed({
+            timeout: 10000,
+            timeoutMsg: `Selector: ${selector} was not displayed within 10000ms`,
+        });
+    }
+
+    protected async waitForElementNotToBeDisplayed(selector: WebdriverIO.Element): Promise<void> {
+        await selector.waitForDisplayed({
+            timeout: 10000,
+            reverse: true,
+            timeoutMsg: `Selector: ${selector} was still displayed within 10000ms`,
+        });
+    }
+
+    protected async scrollToTheTop(): Promise<void> {
+        await browser.execute('window.scrollTo(0, 0)');
+    }
+
+    public async clickOnElement(selector: WebdriverIO.Element): Promise<void> {
+        await selector.click();
+    }
+
+    public async getElementText(selector: WebdriverIO.Element): Promise<string> {
+        return await selector.getText();
+    }
 }
 
 export default BasePage;
